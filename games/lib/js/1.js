c53cff03a46dde297144292f9cd8489858c94777
arifsuhan <suhan10chowdhury@gmail.com>
2021-07-20 14:51:03 +0600

    flip tools ready, need to implement in real game

--- games/lib/js/1.js
+++ games/lib/js/1.js
@@ -1,7 +1,3 @@
-console.log("1");
-
-player_info();
-
 function create_player(player,role,img){
     // parent
     const div_col = document.createElement("div");
@@ -40,17 +36,23 @@ function player_info(){
     var dakat_img = "../lib/img/dakat.png";
     var babu_img = "../lib/img/babu.png";
     // 
-    create_player("Player 1","Chor",chor_img);
-    create_player("Player 2","Police",police_img);
-    create_player("Player 3","Dakat",dakat_img);
-    create_player("Player 4","Babu",babu_img);
-    
+//    create_player("Player 1","Chor",chor_img);
+//    create_player("Player 2","Police",police_img);
+//    create_player("Player 3","Dakat",dakat_img);
+//    create_player("Player 4","Babu",babu_img);    
 }
 
-
+function reveal_id(){
+    console.log("hello rotate");
+    let ele = document.getElementsByClassName("flip-card-inner");
+    ele[0].style.transform = "rotateY(180deg)";
+//    console.log(ele);
+}
 
 //var chor_img = "https://cdn2.vectorstock.com/i/1000x1000/64/21/robber-silhouette-black-vector-8066421.jpg";
 //var police_img = "https://thumbs.dreamstime.com/b/silhouette-policeman-isolated-image-police-officer-illustration-symbol-icon-204509474.jpg";
 //var dakat_img = "https://cdn.xxl.thumbs.canstockphoto.com/robber-silhouette-black-isolated-on-white-background-illustration_csp35477823.jpg";
 //var babu_img = "https://thumbs.dreamstime.com/b/silhouette-dancer-joker-top-hat-dancing-isolated-white-background-144127165.jpg";
 
+//console.log("1");
+//player_info();
